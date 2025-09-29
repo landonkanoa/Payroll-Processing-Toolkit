53782f1b819603932984b25d6c85903a4d8ee27d
Landon <landonlopez@usf.edu>
2025-09-28 21:59:14 -0400

    Steps 2-5 commit

---
+++ cc_5.js
@@ -0,0 +1,47 @@
+//Step 2 //
+const employees = [
+  {
+    name: "Frankie Fisher",
+    hourlyRate: 22,
+    hoursWorked: 40
+  },
+  {
+    name: "Leslie Bell",
+    hourlyRate: 19,
+    hoursWorked: 35
+  },
+  {
+    name: "Ashley Hawkins",
+    hourlyRate: 32,
+    hoursWorked: 42
+  },
+  {
+    name: "Michael Soler",
+    hourlyRate: 28,
+    hoursWorked: 50
+  }
+];
+
+//Step 3//
+function calculateBasePay(rate, hours) {
+  const maxRegularHours = 40;
+  const regularHours = Math.min(hours, maxRegularHours);
+  return rate * regularHours;
+}
+
+//Step 4//
+function calculateOvertimePay(rate, hours) {
+  const maxRegularHours = 40;
+  if (hours <= maxRegularHours) {
+    return 0;
+  }
+  const overtimeHours = hours - maxRegularHours;
+  const overtimeRate = rate * 1.5;
+  return overtimeRate * overtimeHours;
+}
+
+//Step 5//
+function calculateTaxes(grossPay) {
+  const taxRate = .15;
+  return grossPay * taxRate;
+}
